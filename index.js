87c18d4debf08f046428010857524128c8ed2b4d
MariamElZaatari <mariamalzaatari@gmail.com>
2022-02-06 20:26:23 +0200

    Clean JS Code

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ var gender=document.getElementById("gender");
 var age=document.getElementById("age");
 var nationality=document.getElementById("nationality");
 
-function createCountryCard(flagURL, prob){
-    var probability=Math.floor(prob*100);
-    var elements=`<img class="flag" src=${flagURL} alt="TEST"><span>${probability}%</span>`
-    const card=document.createElement("div")
-    card.innerHTML=elements;
-    nationality.append(card);
-}
-
 window.onload=getDogImage();
-async function getDogImage(){
+function getDogImage(){
     var dog_image=document.createElement("img");
-    await fetch(`https://dog.ceo/api/breeds/image/random`)
+    fetch(`https://dog.ceo/api/breeds/image/random`)
     .then(response => response.json())
     .then(data => {
         dog_image.src=data.message;
         document.getElementsByClassName("image")[0].append(dog_image);
     })
 }
-async function prediction(event){
+
+function prediction(event){
+    
     event.preventDefault();
-    var name=document.getElementById("name").value; 
-    await fetch(`https://api.genderize.io?name=${name}`)
+    var name=document.getElementById("name").value;
+    
+    getGender(name);
+    getAge(name);
+    getNationalities(name);
+}
+
+function getGender(name){
+    fetch(`https://api.genderize.io?name=${name}`)
     .then(response => response.json())
     .then(data => {
         if (data.gender=="female"){
@@ -34,26 +35,40 @@ async function prediction(event){
     }).catch(()=>{
         alert("Could not connect to API");
     });
+}
 
-    await fetch(`https://api.agify.io/?name=${name}`)
+function getAge(name){
+    fetch(`https://api.agify.io/?name=${name}`)
     .then(response => response.json())
     .then(data => {
         age.innerText=data.age;
     }).catch(()=>{
         alert("Could not connect to API");
     });
+}
 
-    await fetch(`https://api.nationalize.io/?name=${name}`)
+function getNationalities(name){
+    fetch(`https://api.nationalize.io/?name=${name}`)
     .then(response => response.json())
     .then(async data => {
         var countries=data.country;
         for (var i=0; i<countries.length;i++){
-            await fetch(`https://countryflagsapi.com/png/${countries[i].country_id}`).then(response => {
-                createCountryCard(response.url,countries[i].probability)
-            })
+            var flag_url= await getFlag(countries[i].country_id);
+            createCountryCard(flag_url,countries[i].probability);
         }
     }).catch(()=>{
         alert("Could not connect to API");
     });
+}
 
+async function getFlag(country_id){
+    return await fetch(`https://countryflagsapi.com/png/${country_id}`).then(response => response.url);
+}
+
+function createCountryCard(flagURL, prob){
+    var probability=Math.floor(prob*100);
+    var elements=`<img class="flag" src=${flagURL} alt="TEST"><span>${probability}%</span>`
+    const card=document.createElement("div")
+    card.innerHTML=elements;
+    nationality.append(card);
 }
