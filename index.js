3f930d496ae75546abca8c8a3d1356519c8e5682
MariamElZaatari <mariamalzaatari@gmail.com>
2022-02-06 19:00:11 +0200

    Country Prediction Added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function prediction(event){
         } else{
             gender.innerHTML='<i class="fas fa-male"></i>';
         }
-    }).catch((error)=>{
+    }).catch(()=>{
         alert("Could not connect to API");
     });
 
@@ -21,8 +21,29 @@ async function prediction(event){
     .then(response => response.json())
     .then(data => {
         age.innerText=data.age;
-    }).catch((error)=>{
+    }).catch(()=>{
         alert("Could not connect to API");
     });
-    
+
+    await fetch(`https://api.nationalize.io/?name=${name}`)
+    .then(response => response.json())
+    .then(async data => {
+        var countries=data.country;
+        for (var i=0; i<countries.length;i++){
+            probability=Math.floor(countries[i].probability*100);
+            var country_flag=document.createElement("img");
+            country_flag.classList.add("flag");
+            country_flag.setAttribute("width", "100px");
+            country_flag.setAttribute("height", "70px");
+            await fetch(`https://countryflagsapi.com/png/${countries[i].country_id}`).then(response => {
+                country_flag.src=response.url;
+            })
+            nationality.append(country_flag)
+            nationality.append(probability)
+            nationality.append(document.createElement("br"));
+        }
+    }).catch(()=>{
+        alert("Could not connect to API");
+    });
+
 }
